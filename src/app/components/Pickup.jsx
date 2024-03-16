834f94bdbbd7d8c1cf82d208629fd06f9f5f6d8d
if <trident.ito@gmail.com>
2024-03-16 17:55:43 +0900

    🐛 スライドの修正

--- src/app/components/Pickup.jsx
+++ src/app/components/Pickup.jsx
@@ -5,14 +5,13 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Image from "next/image";
 
-// 独自の矢印コンポーネント
-function NextArrow(props) {
-    const { onClick } = props;
+// 戻る・進むボタン
+const PrevArrow = ({ props }) => {
     return (
-        <div className="absolute top-1/2 right-0 z-10 transform -translate-y-1/2" onClick={onClick}>
+        <div className="absolute top-1/2 left-0 z-10 transform -translate-y-1/2" onClick={props}>
             <Image
-                src="/img/button_next.svg"
-                alt="スライダー用の右矢印（進む）"
+                src="/img/button_prev.svg"
+                alt="スライダー用の左矢印（戻る）"
                 width={40}
                 height={40}
             />
@@ -20,13 +19,12 @@ function NextArrow(props) {
     );
 }
 
-function PrevArrow(props) {
-    const { onClick } = props;
+const NextArrow = ({ props }) => {
     return (
-        <div className="absolute top-1/2 left-0 z-10 transform -translate-y-1/2" onClick={onClick}>
+        <div className="absolute top-1/2 right-0 z-10 transform -translate-y-1/2" onClick={props}>
             <Image
-                src="/img/button_prev.svg"
-                alt="スライダー用の左矢印（戻る）"
+                src="/img/button_next.svg"
+                alt="スライダー用の右矢印（進む）"
                 width={40}
                 height={40}
             />
@@ -39,34 +37,31 @@ const Pickup = () => {
     const settings = {
         dots: true,
         infinite: true,
-        speed: 500,
+        speed: 300,
         slidesToShow: 3, // 中央の画像だけを強調表示
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 3000,
         centerMode: true,
         centerPadding: '0px', // paddingを0にすると画像が中央に来た時のみ大きく表示されます
-        nextArrow: <NextArrow />,
         prevArrow: <PrevArrow />,
-        appendDots: dots => (
-            <div style={{ bottom: "25px" }}>
-                <ul className="flex justify-center space-x-2"> {dots} </ul>
-            </div>
-        ),
-        customPaging: i => (
-            <div className="w-4 h-4 bg-gray-300 rounded-full" />
-        )
+        nextArrow: <NextArrow />,
     };
 
-    // 仮のイメージURLリスト
+    // 表示させる画像
     const images = ["/img/mv_bg-01.jpg", "/img/mv-01.png", "/img/mv-02.png"];
 
     return (
         <div className="relative w-full overflow-hidden">
             <Slider {...settings}>
                 {images.map((image, index) => (
-                    <div key={index} className="outline-none">
-                        <img src={image} alt={`Slide ${index}`} className="mx-auto block" />
+                    <div key={index}>
+                        <Image
+                            src={image}
+                            alt="スライダー用の右矢印（進む）"
+                            fill
+                            className="px-auto block object-contain"
+                        />
                     </div>
                 ))}
             </Slider>
