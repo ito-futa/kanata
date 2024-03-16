1344bd53f66bb7be60aa609f25ab90479ee9d5dc
if <trident.ito@gmail.com>
2024-03-16 12:42:30 +0900

    ボタンのリファクタリング

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 const Button = ({ buttonText, link }) => {
     return (
-        <div className="border-secondary p-1 rounded-full inline-flex justify-center items-center">
-            <a href={link} className="hover:bg-secondary px-16 py-5 rounded-full justify-center items-center gap-8 inline-flex bg-white">
-                <div className="text-secondary hover:text-white text-base font-bold font-['Yu Gothic']">{buttonText} ▶</div>
+        <div className="border-secondary p-1 rounded-full inline-flex justify-center items-center group">
+            <a
+                href={link}
+                className="px-16 py-5 rounded-full justify-center items-center gap-8 inline-flex bg-white group-hover:bg-secondary"
+            >
+                <div className="font-bold font-['Yu Gothic'] text-base">
+                    <span className="text-secondary group-hover:text-white">{buttonText}  ▶</span>
+                </div>
             </a>
         </div>
     );
