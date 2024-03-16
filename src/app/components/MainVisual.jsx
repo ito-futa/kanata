aefd8c94387bf7fba25c90ff93b2fcdc3e4b7ef1
if <trident.ito@gmail.com>
2024-03-16 12:48:28 +0900

    💄 メインビジュアルの出方を調整

--- src/app/components/MainVisual.jsx
+++ src/app/components/MainVisual.jsx
@@ -6,7 +6,7 @@ import GradientText from './GradientText.jsx';
 const MainVisual = () => {
 
     return (
-        <section className="mx-auto pt-28 pb-24 max-w-[1200px] h-[930px] relative bg-white border-8">
+        <section className="mx-auto animate-fadein-1000 pt-28 pb-24 max-w-[1200px] h-[930px] relative bg-white border-8">
             <div className="z-10 max-w-[1000px] h[600px] mx-auto">
                 <Image
                     src="/img/mv_bg-01.jpg"
@@ -18,7 +18,7 @@ const MainVisual = () => {
                     height={600}
                 />
             </div>
-            <div className="z-20 w-1/2 absolute transform -translate-y-1/4 top-[30%] left-1/2 -translate-x-1/2">
+            <div className="z-20 animate-fadein-2000 w-1/2 absolute transform -translate-y-1/4 top-[30%] left-1/2 -translate-x-1/2">
                 <Image
                     src="/img/mv-01.png"
                     className=""
@@ -29,7 +29,7 @@ const MainVisual = () => {
                     objectFit='contain'
                 />
             </div>
-            <div className="animate-fadein-2000 absolute z-30 transform -translate-y-1/4 top-[58%] left-[22%] -translate-x-1/2"><GradientText text="終わらない旅へ" /></div>
+            <div className="animate-fadein-3000 absolute z-30 transform -translate-y-1/4 top-[58%] left-[22%] -translate-x-1/2"><GradientText text="終わらない旅へ" /></div>
             <div className="animate-fadein-4000 absolute z-30 transform -translate-y-1/4 top-[70%] left-[28%] -translate-x-1/2"><GradientText text="ともに出かけよう" /></div>
         </section>
     );
