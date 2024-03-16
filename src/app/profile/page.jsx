24a8b02d1d990cf35b662665830f7a67c1c58d1b
if <trident.ito@gmail.com>
2024-03-17 02:17:04 +0900

    💄 ProfileのUI調整

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -20,7 +20,7 @@ export default function Profile() {
     }, [imageLoading]);
 
     return (
-        <div className="flex justify-center py-32">
+        <div className="flex justify-center py-32 bg-cover bg-no-repeat" style={{ backgroundImage: "url('/img/profile_bg-01.svg')" }}>
             <div className="w-full max-w-6xl flex gap-8">
                 {/* 左カラム：画像 */}
                 <div className={`flex-1 ${!imageLoading && 'animate-fadein-500'}`}>
@@ -35,12 +35,12 @@ export default function Profile() {
                 {/* 右カラム：テキスト */}
                 <div className="flex-1 flex flex-col justify-center">
                     {/* 名前 */}
-                    <div>
+                    <div className='mb-8'>
                         <h1 className="text-[64px] font-bold text-secondary font-['Yu_Gothic']">彩雲カナタ</h1>
                         <h2 className="text-[32px] font-medium text-secondary font-['Yu_Gothic']">Saiun Kanata</h2>
                     </div>
                     {/* 画像リスト */}
-                    <div className="flex gap-2 mt-8">
+                    <div className="flex gap-2 mb-8 ">
                         {[...Array(5)].map((_, i) => (
                             <button key={i} onClick={() => handleIconClick(i + 1)} className="focus:outline-none">
                                 <Image
@@ -53,22 +53,22 @@ export default function Profile() {
                         ))}
                     </div>
                     {/* 説明テキスト */}
-                    <p className="text-base font-medium text-gray-900 mt-4 leading-loose">
+                    <p className="text-base text-accessible mb-8 leading-loose">
                         カメラが好きで、撮影のために秘境を探検するのが趣味。<br />
                         パンケーキも好きで、撮影地の近くに良いカフェがあるかも重要！<br />
                         性格は冒険心があり、新しいことに挑戦するのが大好き。<br />
                         狐耳は自然と動物たちを引き寄せる不思議な魅力を持っている。
                     </p>
                     {/* 情報テーブル */}
-                    <div className="mt-8 flex gap-16 bg-white p-8 rounded-lg border-2 border-grey border-solid">
-                        <div className="text-base font-medium text-accessible">
+                    <div className="flex gap-16 bg-white py-8 px-16 rounded-lg border-2 border-grey border-solid" style={{ width: 'fit-content' }}>
+                        <div className="text-base text-accessible leading-loose">
                             デビュー日<br />
                             誕生日<br />
                             身長<br />
                             年齢<br />
                             好きなカメラ
                         </div>
-                        <div className="text-base font-medium text-accessible">
+                        <div className="text-base text-accessible leading-loose">
                             2024年3月6日<br />
                             11月24日<br />
                             159センチ<br />
@@ -78,6 +78,6 @@ export default function Profile() {
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     );
 }
