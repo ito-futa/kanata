8e7311f287b3e807ca8319b62f249c57fc63c430
if <trident.ito@gmail.com>
2024-03-17 16:39:50 +0900

    💄 Contactページの追加

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,128 @@
+"use client"
+import { useState, useEffect } from 'react';
+import Heading from '../components/Heading';
+
+export default function ContactForm() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [inquiryType, setInquiryType] = useState('');
+    const [content, setContent] = useState('');
+    const [errors, setErrors] = useState({});
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    useEffect(() => {
+        if (isSubmitted) {
+            let formErrors = {};
+            if (!name) formErrors.name = 'お名前を入力してください。';
+            if (email && !email.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i)) formErrors.email = '正しいメールアドレスを入力してください。';
+            if (!inquiryType) formErrors.inquiryType = 'お問い合わせの種類を選択してください。';
+            if (!content) formErrors.content = 'お問い合わせ内容を入力してください。';
+
+            setErrors(formErrors);
+        }
+    }, [isSubmitted, name, email, inquiryType, content]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setIsSubmitted(true);
+
+        if (Object.keys(errors).length === 0) {
+            console.log('Form submitted', { name, email, inquiryType, content });
+            // 送信処理をここで実装
+        }
+    };
+
+    return (
+        <div className="bg-white flex justify-center py-32">
+            <form onSubmit={handleSubmit} className="w-full max-w-lg p-8" style={{ maxWidth: '800px' }}>
+                <div className="mb-12">
+                    <Heading title="Contact" subtitle="お問い合わせ" />
+                </div>
+                <p className="text-center text-lg">
+                    以下の項目をすべて入力して「確認する」ボタンを押してください。
+                </p>
+                <p className="text-red-500 text-center mb-12 text-sm">
+                    ※現在メールを送ることができません
+                </p>
+
+                <div className="mb-12">
+                    <h4 className="text-accessible font-bold mb-2 inline-block mr-2">
+                        ■ お名前</h4>
+                    <span className="bg-red-500 text-white p-1 rounded-md text-sm">必須</span>
+
+                    <input
+                        type="text"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="山田太郎"
+                        className="mt-1 block w-full border-grey border-2 border-solid p-2 rounded-md"
+                    />
+                    {errors.name && <p className="text-red-500 text-xs italic">{errors.name}</p>}
+                </div>
+
+                <div className="mb-12">
+                    <h4 className="text-accessible font-bold mb-2 inline-block mr-2">
+                        ■ ご連絡先メールアドレス
+                    </h4>
+                    <span className="bg-red-500 text-white p-1 rounded-md text-sm">必須</span>
+
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="email@example.com"
+                        className="mt-1 block w-full border-grey border-2 border-solid p-2 rounded-md"
+                    />
+                    {errors.email && <p className="text-red-500 text-xs italic">{errors.email}</p>}
+                </div>
+
+                <div className="mb-12">
+                    <h4 className="text-accessible font-bold mb-2 inline-block mr-2">
+                        ■ お問い合わせの種類
+                    </h4>
+                    <span className="bg-red-500 text-white p-1 rounded-md text-sm">必須</span>
+
+                    <select
+                        value={inquiryType}
+                        onChange={(e) => setInquiryType(e.target.value)}
+                        className="mt-1 block w-full border-grey border-2 border-solid p-2 rounded-md"
+                    >
+                        <option value="">選択して下さい</option>
+                        <option value="video">動画に関してのお問い合わせ</option>
+                        <option value="job">お仕事のお問い合わせ</option>
+                        <option value="fanmail">ファンレター/プレゼントについてのお問い合わせ</option>
+                        <option value="other">その他のお問い合わせ</option>
+                    </select>
+                    {errors.inquiryType && <p className="text-red-500 text-xs italic">{errors.inquiryType}</p>}
+                </div>
+
+                <div className="mb-12">
+                    <h4 className="text-accessible font-bold mb-2 inline-block mr-2">
+                        ■ お問い合わせ内容
+                    </h4>
+                    <span className="bg-red-500 text-white p-1 rounded-md text-sm">必須</span>
+
+                    <textarea
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                        placeholder="お問い合わせ内容をご記入ください"
+                        className="mt-1 block w-full border-grey border-2 border-solid p-2 rounded-md h-32"
+                    />
+                    {errors.content && <p className="text-red-500 text-xs italic">{errors.content}</p>}
+                </div>
+
+                <div className="flex justify-center">
+                    <button type='submit'>
+                        <div className="border-secondary p-1 rounded-full inline-flex justify-center items-center group">
+                            <div className="px-16 py-4 rounded-full justify-center items-center gap-8 inline-flex bg-white group-hover:bg-secondary">
+                                <div className="font-bold font-['Yu Gothic'] text-base">
+                                    <span className="text-secondary group-hover:text-white">送信する</span>
+                                </div>
+                            </div>
+                        </div>
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+}
--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -20,64 +20,67 @@ export default function Profile() {
     }, [imageLoading]);
 
     return (
-        <div className="flex justify-center py-32 bg-cover bg-no-repeat" style={{ backgroundImage: "url('/img/profile_bg-01.svg')" }}>
-            <div className="w-full max-w-6xl flex gap-8">
-                {/* 左カラム：画像 */}
-                <div className={`flex-1 ${!imageLoading && 'animate-fadein-500'}`}>
-                    <Image
-                        width={400}
-                        height={800}
-                        alt="キャラクター画像"
-                        src={`/img/${mainImage}`}
-                        onLoadingComplete={() => setImageLoading(false)}
-                    />
-                </div>
-                {/* 右カラム：テキスト */}
-                <div className="flex-1 flex flex-col justify-center">
-                    {/* 名前 */}
-                    <div className='mb-8'>
-                        <h1 className="text-[64px] font-bold text-secondary font-['Yu_Gothic']">彩雲カナタ</h1>
-                        <h2 className="text-[32px] font-medium text-secondary font-['Yu_Gothic']">Saiun Kanata</h2>
+        <>
+            <div className="flex justify-center py-32 bg-left bg-bottom bg-no-repeat w-screen" style={{ backgroundImage: "url('/img/profile_bg-01.svg')" }}>
+                <div className="w-full max-w-5xl flex gap-8">
+                    {/* 左カラム：画像 */}
+                    <div className={`flex-1 ${!imageLoading && 'animate-fadein-500'}`}>
+                        <Image
+                            width={350}
+                            height={700}
+                            alt="キャラクター画像"
+                            src={`/img/${mainImage}`}
+                            onLoadingComplete={() => setImageLoading(false)}
+                        />
                     </div>
-                    {/* 画像リスト */}
-                    <div className="flex gap-2 mb-8 ">
-                        {[...Array(5)].map((_, i) => (
-                            <button key={i} onClick={() => handleIconClick(i + 1)} className="focus:outline-none">
-                                <Image
-                                    width={100}
-                                    height={100}
-                                    alt={`彩雲カナタのアイコン${i + 1}`}
-                                    src={`/img/chara_icon-0${i + 1}.png`}
-                                />
-                            </button>
-                        ))}
-                    </div>
-                    {/* 説明テキスト */}
-                    <p className="text-base text-accessible mb-8 leading-loose">
-                        カメラが好きで、撮影のために秘境を探検するのが趣味。<br />
-                        パンケーキも好きで、撮影地の近くに良いカフェがあるかも重要！<br />
-                        性格は冒険心があり、新しいことに挑戦するのが大好き。<br />
-                        狐耳は自然と動物たちを引き寄せる不思議な魅力を持っている。
-                    </p>
-                    {/* 情報テーブル */}
-                    <div className="flex gap-16 bg-white py-8 px-16 rounded-lg border-2 border-grey border-solid" style={{ width: 'fit-content' }}>
-                        <div className="text-base text-accessible leading-loose">
-                            デビュー日<br />
-                            誕生日<br />
-                            身長<br />
-                            年齢<br />
-                            好きなカメラ
+                    {/* 右カラム：テキスト */}
+                    <div className="flex-1 flex flex-col justify-center">
+                        {/* 名前 */}
+                        <div className='mb-8'>
+                            <h1 className="text-[64px] font-bold text-secondary font-['Yu_Gothic']">彩雲カナタ</h1>
+                            <h2 className="text-[32px] font-medium text-secondary font-['Yu_Gothic']">Saiun Kanata</h2>
                         </div>
-                        <div className="text-base text-accessible leading-loose">
-                            2024年3月6日<br />
-                            11月24日<br />
-                            159センチ<br />
-                            ??歳<br />
-                            富士フィルム
+                        {/* 画像リスト */}
+                        <div className="flex gap-2 mb-8 ">
+                            {[...Array(5)].map((_, i) => (
+                                <button key={i} onClick={() => handleIconClick(i + 1)} className="focus:outline-none">
+                                    <Image
+                                        width={100}
+                                        height={100}
+                                        alt={`彩雲カナタのアイコン${i + 1}`}
+                                        src={`/img/chara_icon-0${i + 1}.png`}
+                                    />
+                                </button>
+                            ))}
+                        </div>
+                        {/* 説明テキスト */}
+                        <p className="text-base text-accessible mb-8 leading-loose">
+                            カメラが好きで、撮影のために秘境を探検するのが趣味。<br />
+                            パンケーキも好きで、撮影地の近くに良いカフェがあるかも重要！<br />
+                            性格は冒険心があり、新しいことに挑戦するのが大好き。<br />
+                            狐耳は自然と動物たちを引き寄せる不思議な魅力を持っている。
+                        </p>
+                        {/* 情報テーブル */}
+                        <div className="flex gap-16 bg-white py-8 px-16 rounded-lg border-2 border-grey border-solid" style={{ width: 'fit-content' }}>
+                            <div className="text-base text-accessible leading-loose">
+                                デビュー日<br />
+                                誕生日<br />
+                                身長<br />
+                                年齢<br />
+                                好きなカメラ
+                            </div>
+                            <div className="text-base text-accessible leading-loose">
+                                2024年3月6日<br />
+                                11月24日<br />
+                                159センチ<br />
+                                ??歳<br />
+                                富士フィルム
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div >
+            </div >
+            <div></div>
+        </>
     );
 }
